187903d95fa11a596a8401fcf5d0a68173b15091
Sabarno Chatterjee <sabarnochatterjee@gmail.com>
2025-09-23 19:49:40 +0530

    docs: add README for variables and oeperators

---
+++ foundations/04-Javascript-Basics/01-variables-and-operators/javascript.js
@@ -0,0 +1,39 @@
+console.log("Hello, World!!!")
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+console.log(name+" "+surname);
+
+let age = 11;
+console.log(age);
+
+age=56;
+
+console.log(age);
+
+const pi = 3.14;
+
+console.log(pi);
+console.log((3+2) - 76 * (1+1));
+console.log(6 - '2');
+console.log('8'-2);
+console.log('6' / '2');
+console.log('6'/2);
+// alert(5%2);
+// console.log(5**2);
+
+
+let apples = "2";
+let oranges = "3";
+
+alert(apples + oranges);
+alert(+apples + +oranges);
+
+let a=1;
+let b=2;
+
+let c=3-(a=b+1);
+alert(a);
+alert(c);
