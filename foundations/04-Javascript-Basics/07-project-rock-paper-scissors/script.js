8a0acdb940385155705f09c46948c8d50644bf06
Sabarno Chatterjee <sabarnochatterjee@gmail.com>
2025-10-01 21:35:30 +0530

    refactor: restructure Rock Paper Scissors project with playGame() and printScore() functions

--- foundations/04-Javascript-Basics/07-project-rock-paper-scissors/script.js
+++ foundations/04-Javascript-Basics/07-project-rock-paper-scissors/script.js
@@ -1,16 +1,22 @@
 function getComputerChoice() {
-  return Math.floor(Math.random() * 3);
+  let computerChoice = Math.floor(Math.random() * 3);
+  console.log(`Computer: ${computerChoice}`);   
+  return computerChoice;
 }
+
 
 function getHumanChoice() {
   let humanChoice = prompt("Enter 0 for rock, 1 for paper or 2 for scissor.");
+  console.log(`Human: ${humanChoice}`);
   return humanChoice;
 }
 
+// GLOBAL VARIABLES
 let humanScore = 0,
-  computerScore = 0;
+  computerScore = 0,
+  bestOf = 5;
 
-function playRound(computerChoice, humanChoice) {
+  function playRound(computerChoice, humanChoice) {
   if (computerChoice == humanChoice) return "Tie";
   if (computerChoice === 0 && humanChoice === 1) {
     humanScore++;
@@ -31,21 +37,26 @@ function playRound(computerChoice, humanChoice) {
   }
 }
 
-let bestOf = 5;
-while (bestOf) {
-  let humanChoice = Number(getHumanChoice());
-  let computerChoice = getComputerChoice();
-
-  let result = playRound(computerChoice, humanChoice);
-
-  console.log(result);
-  console.log(`Human: ${humanScore}, Computer: ${computerScore}`);
-  bestOf--;
+function printScore() {
+  if (humanScore > computerScore) console.log("🎉 Human wins the game!");
+  else if (computerScore > humanScore)
+    console.log("💻 Computer wins the game!");
+  else console.log("🤝 It's a tie overall!");
 }
 
-if(humanScore>computerScore)
-    console.log("🎉 Human wins the game!");
-else if(computerScore>humanScore)
-    console.log("💻 Computer wins the game!");
-else
-    console.log("🤝 It's a tie overall!");
+function playGame() {
+  while (bestOf) {
+    let humanChoice = Number(getHumanChoice());
+    let computerChoice = getComputerChoice();
+
+    let result = playRound(computerChoice, humanChoice);
+
+    console.log(result);
+    console.log(`Human: ${humanScore}, Computer: ${computerScore}`);
+
+    bestOf--;
+  }
+  printScore();
+}
+
+playGame();
