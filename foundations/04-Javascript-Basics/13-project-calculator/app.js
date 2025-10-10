2c8343da02e6d686babd6c89ed38d6f5f21cb3a7
Sabarno Chatterjee <sabarnochatterjee@gmail.com>
2025-10-10 16:47:36 +0530

    feat: implement calculator display functional

--- foundations/04-Javascript-Basics/13-project-calculator/app.js
+++ foundations/04-Javascript-Basics/13-project-calculator/app.js
@@ -5,7 +5,9 @@ const display = document.querySelector(".display");
 const keys = document.querySelector(".keys");
 const key = document.querySelector(".key");
 
-result = 0;
+let result = 0;
+let num1 = 0;
+let num2 = 0;
 
 function add(operand1, operand2) {
   return (Number(operand1) + Number(operand2)).toFixed(2);
@@ -29,26 +31,41 @@ function operate(num1, operator, num2) {
     case "+":
       result = add(num1, num2);
       console.log(result);
+      displayValue(result);
       break;
     case "-":
       result = subtract(num1, num2);
       console.log(result);
+      displayValue(result);
       break;
     case "*":
       result = multiply(num1, num2);
       console.log(result);
+      displayValue(result);
       break;
     case "/":
       result = divide(num1, num2);
       console.log(result);
+      displayValue(result);
       break;
     case "^":
       result = power(num1, num2);
       console.log(result);
+      displayValue(result);
       break;
     default:
       console.log("Not a valid operation!");
   }
 }
 
+function displayValue(number) {
+  const value = document.createElement("h1");
+  const insertionPoint = document.createElement("h1");
+  value.innerText = `${result} `;
+  insertionPoint.classList.add("insertionPoint");
+  insertionPoint.innerText = "|";
+  display.appendChild(value);
+  display.appendChild(insertionPoint);
+}
 
+operate(2, "^", 4);
