5550b8848e5a350ab3822f24da46e09bed431b15
Sabarno Chatterjee <sabarnochatterjee@gmail.com>
2025-10-10 16:21:11 +0530

    feat: add operate function to handle arithmetic operations

--- foundations/04-Javascript-Basics/13-project-calculator/app.js
+++ foundations/04-Javascript-Basics/13-project-calculator/app.js
@@ -2,24 +2,53 @@
 const body = document.querySelector("body");
 const calculator = document.querySelector(".calculator");
 const display = document.querySelector(".display");
-const keys =document.querySelector(".keys");
-const key =document.querySelector(".key");
+const keys = document.querySelector(".keys");
+const key = document.querySelector(".key");
 
-function add(operand1,operand2){
-    return (Number(operand1)+Number(operand2)).toFixed(2);
+result = 0;
+
+function add(operand1, operand2) {
+  return (Number(operand1) + Number(operand2)).toFixed(2);
 }
-function subtract(operand1,operand2){
-    return (Number(operand1)-Number(operand2)).toFixed(2);
+function subtract(operand1, operand2) {
+  return (Number(operand1) - Number(operand2)).toFixed(2);
 }
-function multiply(operand1,operand2){
-    return (Number(operand1)*Number(operand2)).toFixed(2);
+function multiply(operand1, operand2) {
+  return (Number(operand1) * Number(operand2)).toFixed(2);
 }
-function divide(operand1,operand2){
-    return (Number(operand1)/Number(operand2)).toFixed(2);
+function divide(operand1, operand2) {
+  return (Number(operand1) / Number(operand2)).toFixed(2);
 }
 
-function power(base,exponent){
-    return (Math.pow(base,exponent)).toFixed(2);
+function power(base, exponent) {
+  return Math.pow(base, exponent).toFixed(2);
+}
+
+function operate(num1, operator, num2) {
+  switch (operator) {
+    case "+":
+      result = add(num1, num2);
+      console.log(result);
+      break;
+    case "-":
+      result = subtract(num1, num2);
+      console.log(result);
+      break;
+    case "*":
+      result = multiply(num1, num2);
+      console.log(result);
+      break;
+    case "/":
+      result = divide(num1, num2);
+      console.log(result);
+      break;
+    case "^":
+      result = power(num1, num2);
+      console.log(result);
+      break;
+    default:
+      console.log("Not a valid operation!");
+  }
 }
 
 
